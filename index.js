3d855f207577f0b4a72b12f623d2f14a236a2cb4
Samar Shrestha <66566474+samarshrestha720@users.noreply.github.com>
2023-10-26 19:22:56 +0545

    test for 3.13

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
+require("dotenv").config();
+
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
 
+const Person = require("./models/person");
+
 app.use(cors());
 app.use(express.static("dist"));
+
 morgan.token("data", function getData(req) {
   return JSON.stringify(req.body);
 });
 
 app.use(express.json());
 app.use(morgan(":method :url :response-time :data"));
-
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -48,17 +30,15 @@ app.get("/api/info", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  Person.findById(request.params.id).then((note) => {
+    response.json(note);
+  });
 });
 
 app.post("/api/persons", (request, response) => {
@@ -68,25 +48,23 @@ app.post("/api/persons", (request, response) => {
       error: "content missing",
     });
   }
+  // const check = persons.map((person) =>
+  //   person.name == body.name ? true : null
+  // );
+  // if (check.includes(true)) {
+  //   return response.status(400).json({
+  //     error: "name already exists in phonebook",
+  //   });
+  // }
 
-  const check = persons.map((person) =>
-    person.name == body.name ? true : null
-  );
-  if (check.includes(true)) {
-    return response.status(400).json({
-      error: "name already exists in phonebook",
-    });
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: Math.round(100 * Math.random()),
-  };
+  });
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -95,7 +73,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
